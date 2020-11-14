9aeabb6d19a2d2c72123f8d384057f1ae7fc2396
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-11-13 21:37:37 -0300

    organizando componentes para exibição

--- src/components/basicos/ComParametro.jsx
+++ src/components/basicos/ComParametro.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ComParametro({titulo, subtitulo}) {
     return ( 
         <div>
+            <h1> Segundo Componente </h1>
             <h2>
                 <strong>{titulo}</strong>
             </h2>
