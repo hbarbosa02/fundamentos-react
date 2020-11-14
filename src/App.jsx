13a7842f64f2a7c3d454564fa91c7c38a1417650
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-11-14 12:14:41 -0300

    compreendendo flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,29 @@ import Fragmento from './components/basicos/Fragmento'
 import NumAleatorio from './components/basicos/NumAleatorio'
 import Card from './components/layout/Card'
 
+import './App.css'
+
 const App = (_) => {
   return (
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React (Arrow)</h1>
-        
-        <Card titulo="#04 - Desafio Aleatório">
-          <NumAleatorio min={0} max={10} />
-        </Card>
+        <div className="Cards">
+          <Card titulo="#04 - Desafio Aleatório">
+            <NumAleatorio min={0} max={10} />
+          </Card>
 
-        <Card titulo="#03 - Fragmento">
-          <Fragmento />
-        </Card>
-        
-        <Card titulo="#02 - Com Parâmetros"> 
-          <ComParametro titulo={'Componentes'} subtitulo={'Com Parâmetro'} />
-        </Card>
-        
-        <Card titulo="#01 - Primeiro Componente">
-          <Primeiro />
-        </Card>
+          <Card titulo="#03 - Fragmento">
+            <Fragmento />
+          </Card>
+          
+          <Card titulo="#02 - Com Parâmetros"> 
+            <ComParametro titulo={'Componentes'} subtitulo={'Com Parâmetro'} />
+          </Card>
+          
+          <Card titulo="#01 - Primeiro Componente">
+            <Primeiro />
+          </Card>
+        </div>
     </div>
   );
 }
